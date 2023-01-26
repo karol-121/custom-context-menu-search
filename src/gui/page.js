cb03958bd44c06f18caa2ad6da90cb753bb9a1bd
karol-121 <36259145+karol-121@users.noreply.github.com>
2023-01-26 19:42:15 +0100

    gui fixes

--- src/gui/page.js
+++ src/gui/page.js
@@ -123,7 +123,7 @@ function createRow(title, context, url) {
 	const cell_manage = document.createElement('td');
 	const del_button = document.createElement('button');
 		del_button.addEventListener('click', function () { deleteRow(row) }, false);
-		del_button.append("delete row");
+		del_button.append("Delete search");
 	cell_manage.appendChild(del_button);
 	
 	row.appendChild(cell_title);
